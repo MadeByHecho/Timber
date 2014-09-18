bd304e8fa0a302f03ccf9e524db205ee4a70e690
Scott Petit <petit.scott@gmail.com>
2014-09-18 20:30:37 +0300

    Initial Commit

---
+++ Timber/Timber.h
@@ -0,0 +1,19 @@
+//
+//  Timber.h
+//  Timber
+//
+//  Created by Scott Petit on 9/18/14.
+//  Copyright (c) 2014 Scott Petit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Timber.
+FOUNDATION_EXPORT double TimberVersionNumber;
+
+//! Project version string for Timber.
+FOUNDATION_EXPORT const unsigned char TimberVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Timber/PublicHeader.h>
+
+
